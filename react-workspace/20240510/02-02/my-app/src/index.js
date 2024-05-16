b380a1a88d071725e58b2ac6fbec77432d0d7fbe
hansol <ryu.axel@gmail.com>
2024-05-16 13:52:46 +0900

    finish clock

--- react-workspace/20240510/02-02/my-app/src/index.js
+++ react-workspace/20240510/02-02/my-app/src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Clock from "./chapter_04/Clock";
 
 setInterval(() => {
     ReactDOM.render(
-    <React.StrictMode>
-        <Clock />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
-    }, 1000)
+        <React.StrictMode>
+            <Clock />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}, 1000);
 
 
 // If you want to start measuring performance in your app, pass a function
